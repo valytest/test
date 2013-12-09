5378bb9e798d8446a29120b8c168067f630425fb
valy <valyamariei@yahoo.com>
2013-12-09 16:07:39 +0200

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Changes after first commit!");
+	System.out.println("Changes after Pushing to remote!");
     }
 }
